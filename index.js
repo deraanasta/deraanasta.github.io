6f9bec9d1578d12dbe6e76e7e2ebf1c2062e77a5
deraanasta <152891393+deraanasta@users.noreply.github.com>
2024-01-31 19:56:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 });
 
 function validateForm() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  var username = document.getElementById("tyas").value;
+  var password = document.getElementById("28april2009").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
   if (username === "tyas" && password === "28april2009") {
